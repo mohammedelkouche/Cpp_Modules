2a9c7b8daa3441e75b73f2b7564e9490582fa2a3
mohammedelkouche <mohammed0elkouche@gmail.com>
2023-10-22 19:12:19 +0100

    construct the classes

---
+++ module 00/ex01/Contact.hpp
@@ -0,0 +1,8 @@
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+// Class CONTACT_HPP
+// {
+//     private:
+        
+// }
---
+++ module 00/ex01/PhoneBook.hpp
@@ -0,0 +1,10 @@
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include <iostream>
+
+// Class PHONEBOOK
+// {
+//     private:
+
+// }
